0ae12861dfac3363a0ecf38e6a532c22b21819bd
SizweMalobola <25reyals@gmail.com>
2021-11-20 19:58:46 +0200

    Add toggleForm function logic toggleForm will toggle the value of display on FormModal component

--- src/components/AddTaskButton.js
+++ src/components/AddTaskButton.js
@@ -2,7 +2,14 @@ import React from "react";
 import styles from "./add-task-button.module.css";
 function AddTaskButton() {
   // this function toggles the display for the form for adding tasks
-  function toggleForm() {}
+  function toggleForm() {
+    let targetElement = document.querySelector("#form-modal");
+    if (targetElement.style.display === "flex") {
+      targetElement.style.display = "none";
+    } else {
+      targetElement.style.display = "flex";
+    }
+  }
 
   return (
     <button className={styles["task-button"]} onClick={toggleForm}>
