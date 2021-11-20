21a1d8b272695300b2bf588f0462bea2737ef38c
SizweMalobola <25reyals@gmail.com>
2021-11-20 23:20:50 +0200

    Add FormModal functionality After the FormModal has been filled in its sent as an object The object is sent to Main component and stored in Tasks state array

--- src/components/FormModal.js
+++ src/components/FormModal.js
@@ -1,13 +1,57 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styles from "./form-modal.module.css";
 
-function FormModal() {
+function FormModal(props) {
+  // create a state variable that will store the values the title and description
+  const [title, setTitle] = useState(null);
+  const [description, setDescription] = useState(null);
+  let titleRef = useRef(null);
+  let descriptionRef = useRef(null);
+  let formModalRef = useRef(null);
   return (
-    <form id="form-modal" className={styles["form-container"]}>
+    <form
+      id="form-modal"
+      ref={formModalRef}
+      className={styles["form-container"]}
+    >
       <h1>Add Task</h1>
-      <input type="text" placeholder="Give your task a title" required={true} />
-      <textarea rows="5" placeholder="Description for your task" />
-      <button>Done</button>
+      <input
+        type="text"
+        placeholder="Give your task a title"
+        ref={titleRef}
+        onChange={(e) => {
+          setTitle(e.target.value);
+        }}
+      />
+      <textarea
+        rows="5"
+        placeholder="Description for your task"
+        ref={descriptionRef}
+        onChange={(e) => {
+          setDescription(e.target.value);
+        }}
+      />
+      <button
+        onClick={(e) => {
+          // prevent page refresh
+          e.preventDefault();
+          if (titleRef.current.value && descriptionRef.current.value) {
+            //if both form inputs are present save object
+            props.addTask({
+              title: title,
+              description: description,
+              complete: false,
+            });
+            // reset input values
+            titleRef.current.value = null;
+            descriptionRef.current.value = null;
+            // hide form modal
+            formModalRef.current.style.display = "none";
+          }
+        }}
+      >
+        Done
+      </button>
     </form>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import FormModal from "./FormModal";
 import styles from "./main-section.module.css";
 function Main() {
+  // create a function that will be passed as props to FormModal
+  // function will take a object params {title:"" discription:"" completed:false}
+  // and save it inside tasks(array) state varible.
+  const [tasks, setTasks] = useState([]);
+  const addTask = (obj) => {
+    // set index on every individual task
+    obj.index = tasks.length - 1 < 0 ? 0 : tasks.length - 1;
+    setTasks((state) => [...state, obj]);
+  };
+  console.log(tasks);
   return (
     <main className={styles["main-section-container"]}>
       <h3>Tasks</h3>
-      <FormModal />
+      <FormModal addTask={addTask} />
     </main>
   );
 }
