afc08c9367d0b7d6d05f2d9b0f60a501d4f6e4a4
SizweMalobola <25reyals@gmail.com>
2021-11-20 18:01:11 +0200

    Add Main componet along with its style module

---
+++ src/components/Main.js
@@ -0,0 +1,7 @@
+import React from "react";
+import styles from "./main-section.module.css";
+function Main() {
+  return <main className={styles["main-section-container"]}></main>;
+}
+
+export default Main;
